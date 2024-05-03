ccacc367f1d10d3b9bed201cf8f6ab6beb82634f
ElKoloWirz96 <agustin-96@live.com.ar>
2024-05-02 21:51:59 -0300

    Empiexo a hacer el routing de la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { AutchComponent } from './layouts/autch/autch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'autch',
+    component: AutchComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layouts/dashboard/pages/hom/hom.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomComponent } from './hom.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomModule { }
