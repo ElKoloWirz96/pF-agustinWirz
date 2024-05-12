d0d842ea2644d5c8ebfb744b4d774b59c1e87a69
ElKoloWirz96 <agustin-96@live.com.ar>
2024-05-12 15:33:03 -0300

    Empiezo a modificar y crear el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    loadChildren: () => 
+      import ('./layouts/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule )
   },
   {
     path: 'autch',
--- src/app/layouts/dashboard/dashboard-routing.module.ts
+++ src/app/layouts/dashboard/dashboard-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./pages/users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./pages/products/products.module').then((m) => m.ProductsModule),
+  },
+  {
     path: '',
-    children: [
-      {
-        path: 'home',
-        loadChildren: () => 
-          import ('./pages/home/home.module').then( (m) => m.HomeModule ),
-      }
-    ]
+    pathMatch: 'full',
+    redirectTo: 'home',
   }
 ];
 
